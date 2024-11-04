ac1f1e93b70eca70ba5f207bbfa3bd6cc3d0d786
ptitjbus <ptitjbus02@gmail.com>
2024-11-05 00:03:48 +0100

    feat: Style and little adds

--- src/Repository/CategoryRepository.php
+++ src/Repository/CategoryRepository.php
@@ -19,20 +19,18 @@ class CategoryRepository extends ServiceEntityRepository
 
     // Version alternative plus performante
 
-    //    /**
-    //     * @return Category[] Returns an array of Category objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('c')
-    //            ->andWhere('c.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('c.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+        /**
+         * @return Category[] Returns an array of Category objects
+         */
+        public function findThreeLatestCategories(): array
+        {
+            return $this->createQueryBuilder('c')
+                ->orderBy('c.id', 'DESC')
+                ->setMaxResults(3)
+                ->getQuery()
+                ->getResult()
+            ;
+        }
 
     //    public function findOneBySomeField($value): ?Category
     //    {
